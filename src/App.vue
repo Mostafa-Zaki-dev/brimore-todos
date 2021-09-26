26111e01091144a3afbca3631013177a08cb07c4
Mostafa Zaki <mostafadashzaki@gmail.com>
2021-09-26 09:33:02 +0200

    API integration

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class='todo-app'>
+    <h1>What's the Plan for Today?</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  // components: {},
+  setup() {
+   
   },
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style >
 </style>
---
+++ src/core/index.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const url = "https://jsonplaceholder.typicode.com/todos";
+
+export default {
+  async fetchTodos() {
+    const { data } = await axios.get(url);
+    console.log("fetchTodos data: ", data);
+    return data;
+  },
+  async addTodo(todo) {
+    const { data } = await axios.post(url, todo);
+    // return new todo body with its assigned id
+    return data;
+  },
+  async updateTodo(id, todo) {
+    const { data } = await axios.put(`${url}/${id}`, todo);
+    console.log(data);
+    // return new todo body with id
+    return data;
+  },
+  async deleteTodo(id) {
+    const { data } = await axios.put(`${url}/${id}`);
+    // return {id: id}
+    return data;
+  },
+};
